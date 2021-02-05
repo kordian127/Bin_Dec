81cb2adcb38a877e6c5d8678681a58d8ead5c60e
kordian127 <kordian126@gmail.com>
2021-02-05 11:46:00 +0100

    dectobin done

--- BinDec.cpp
+++ BinDec.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 #include<conio.h>
+#include<vector>
 using namespace std;
+
+
+void DecToBin(int dec){
+    vector<int> x;
+
+    while(dec!=0){
+        x.push_back(dec%2);
+        dec/=2;
+    }
+    for(int i=x.size()-1;i>-1;i--){
+        cout<<x[i];
+    }
+}
 
 
 
@@ -15,7 +29,9 @@ while(true){
             cout<<"Dec to Bin"<<endl;
             cout<<"Enter number: ";
             cin>>numb;
-            cout<<numb<<"->"<<endl;
+            cout<<numb<<"->";
+            DecToBin(numb);
+            cout<<endl;
             cout<<"Press any key";
             reset=getch();
             system("CLS");
@@ -23,6 +39,12 @@ while(true){
 
         case 50:
             cout<<"Bin to Dec"<<endl;
+            cout<<"Enter number: ";
+            cin>>numb;
+            cout<<numb<<"->"<<endl;
+            cout<<"Press any key";
+            reset=getch();
+            system("CLS");
             break;
 
         case 51:
