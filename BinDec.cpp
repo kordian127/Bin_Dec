60752f76e5139f3b7a8c7621673aba1c8bcbc3c2
kordian127 <kordian126@gmail.com>
2021-02-05 11:21:38 +0100

    update

--- BinDec.cpp
+++ BinDec.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+
+
+int main(){
+int choose,numb,reset;
+
+while(true){
+    cout<<"1.Decimal to Binary"<<endl<<"2.Binary to Decimal"<<endl<<"3.Exit"<<endl;
+    choose = getch();
+    switch(choose){
+        case 49:
+            cout<<"Dec to Bin"<<endl;
+            cout<<"Enter number: ";
+            cin>>numb;
+            cout<<numb<<"->"<<endl;
+            cout<<"Press any key";
+            reset=getch();
+            system("CLS");
+            break;
+
+        case 50:
+            cout<<"Bin to Dec"<<endl;
+            break;
+
+        case 51:
+            return 0;
+
+        default:
+            cout<<"Wrong choice"<<endl;
+            break;
+    }
+}
+return 0;
+}
